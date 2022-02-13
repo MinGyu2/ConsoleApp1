18647b28da1103871928c1604557b955669e0018
MinQ <MinQ@DESKTOP-1I1G0BH>
2022-02-13 16:13:37 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Cacu{
+    class Calculate {
+        public static double DoOperation(double a,double b,String s) {
+            double result = double.NaN;
+
+            switch (s) {
+                case "a":
+                    result = a + b;
+                    break;
+                case "s":
+                    result = a - b;
+                    break;
+                case "m":
+                    result = a * b;
+                    break;
+                case "d":
+                    if (b == 0)
+                        break;
+                    result = a / b;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+    class Prog{
+        static void Main(String[] args) {
+            bool endApp = false;
+            while (!endApp) {
+                double num1, num2;
+                string n1="", n2="";
+                do {
+                    Console.WriteLine("첫 번째 숫자를 입력해 주세요!");
+                    n1 = Console.ReadLine();
+                } while (!double.TryParse(n1, out num1));
+
+                do {
+                    Console.WriteLine("두 번째숫자를 입력해 주세요!");
+                    n2 = Console.ReadLine();
+                }while(!double.TryParse(n2, out num2));
+
+                Console.WriteLine("a,s,m,d");
+                string a= Console.ReadLine();
+                double re = Calculate.DoOperation(num1, num2, a);
+                Console.WriteLine($"결과: {re}");
+
+                Console.WriteLine("끝내게 습니까?(y/n)");
+                if(Console.ReadLine() == "y") endApp=true;
+
+                Console.WriteLine("======================================================\n");
+            }
+        }
+    }
+}
